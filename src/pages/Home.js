f223d137bc67861bab56f4dfdc0c13a38d27c169
AKOWSPC <akows141@gmail.com>
2022-12-13 14:39:49 +0900

    [메인 페이지] - 메인 페이지 문구 삽입. - 푸터 버튼 및 반응형 UI 적용.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,18 +9,39 @@ const Home = () => {
         titleElement.innerHTML = '원신프로젝트';
     }, []);
 
+    const moveSony = () => {
+        window.open('https://www.playstation.com/ko-kr/games/genshin-impact/');
+    }
+    const moveAppS = () => {
+        window.open('https://apps.apple.com/app/id1517783697');
+    }
+    const moveGooS = () => {
+        window.open('https://play.google.com/store/apps/details?id=com.miHoYo.GenshinImpact');
+    }
+    const moveWinS = () => {
+        window.open('https://genshin.hoyoverse.com/ko/news/detail/5752');
+    }
+
     return (
         <>
-            <div className='home'>
+            <div className='home'/>
 
-            </div>
+            <div className='homecontents'>
+                <div className='maintext'>
+                    <p>《원신》은 게임 개발사 미호요가 개발한 오픈 월드 어드벤처 게임입니다.</p>
+                    <p>이 원소의 힘으로 가득 찬 판타지 대륙 「티바트」에는 </p>
+                    <p>어떤 이야기들이 모험가들을 기다리고 있을까요? </p>
+                    <p>귀여운 페이몬과 함께 이 광활한 대지로 모험을 떠나보세요!</p>
+                </div>
 
-            <div className='text'>
-                <p>메인화면 텍스트</p>
-                <p>메인화면 텍스트</p>
-                <p>메인화면 텍스트</p>
-                <p>메인화면 텍스트</p>
-                <p>메인화면 텍스트</p>
+                <div className='paimon'/>
+                
+                <div className='mainfooter'>
+                    <span className='footerbtn1' onClick={moveWinS}/>
+                    <span className='footerbtn2' onClick={moveGooS}/>
+                    <span className='footerbtn3' onClick={moveAppS}/>
+                    <span className='footerbtn4' onClick={moveSony}/>
+                </div>
             </div>
         </>
     )
