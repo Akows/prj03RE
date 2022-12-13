64e70e7a6da224a08c6c7ddf77e3d311bd8d48d8
AKOWSPC <akows141@gmail.com>
2022-12-13 18:13:09 +0900

    [게임정보 페이지] - 메뉴 리모콘 UI 구현.

--- src/pages/GameInfo.js
+++ src/pages/GameInfo.js
@@ -1,20 +1,49 @@
 import '../ResetStyle.css';
 import './GameInfo.css';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const GameInfo = () => {
+
+    const [remoteOn, setRemoteOn] = useState(false);
+
+    const handleScrollCharacter = (event) => {
+        if (event.target.documentElement.scrollTop >= 1100) {
+            setRemoteOn(true);
+        }
+        else {
+            setRemoteOn(false);
+        }
+    };
+
     useEffect(() => {
         const titleElement = document.getElementsByTagName('title')[0];
         titleElement.innerHTML = '게임정보';
+
+        window.addEventListener('scroll', handleScrollCharacter); // eslint-disable-next-line
     }, []);
 
     return (
         <>
             <div className='gameinfo'>
+
+                <div className={['gameinforemote', `remoteOn_${remoteOn}`].join(' ')}>
+                    <div>
+                        d
+                    </div>
+                    <div>
+                        d
+                    </div>
+                    <div>
+                        d
+                    </div>
+                    <div>
+                        d
+                    </div>
+                </div>
                 
                 <div className='gametrailer'>
-                    <iframe src="https://www.youtube.com/embed/yytFBFzJLmU" title="원신 | 미호요 《원신》 트레일러 영상 첫 공개" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/yytFBFzJLmU" title="원신 | 미호요 《원신》 트레일러 영상 첫 공개" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
                 </div>
 
                 <div className='gameslides'>
