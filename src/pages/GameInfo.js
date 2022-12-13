ace7b41e685f76b7f36f6a84ad5cfd97b739b838
AKOWSPC <akows141@gmail.com>
2022-12-13 17:31:19 +0900

    [게임정보 페이지] - UI 구현 시작. - 게임 트레일러 영역 구현.

--- src/pages/GameInfo.js
+++ src/pages/GameInfo.js
@@ -12,7 +12,19 @@ const GameInfo = () => {
     return (
         <>
             <div className='gameinfo'>
-                gameinfo
+                
+                <div className='gametrailer'>
+                    <iframe src="https://www.youtube.com/embed/yytFBFzJLmU" title="원신 | 미호요 《원신》 트레일러 영상 첫 공개" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+                <div className='gameslides'>
+                    
+                </div>
+
+                <div className='gameintroduce'>
+
+                </div>
+
             </div>
         </>
     )
